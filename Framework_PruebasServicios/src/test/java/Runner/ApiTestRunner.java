bd92a1ad85378b35f8b79e61fde5e378ee4a1611
erickrojas <rojaserick.g@gmail.com>
2026-09-24 11:01:20 -0300

    feat: habilitar tags dinamicos de Cucumber

--- Framework_PruebasServicios/src/test/java/Runner/ApiTestRunner.java
+++ Framework_PruebasServicios/src/test/java/Runner/ApiTestRunner.java
@@ -5,7 +5,6 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
@@ -15,10 +14,6 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @ConfigurationParameter(
         key = GLUE_PROPERTY_NAME,
         value = "StepDefinition"
-)
-@ConfigurationParameter(
-        key = FILTER_TAGS_PROPERTY_NAME,
-        value = "@health"
 )
 @ConfigurationParameter(
         key = PLUGIN_PROPERTY_NAME,
